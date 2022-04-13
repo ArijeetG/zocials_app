5134baa934d316b358d8f6473c52c9281c99c7c3
prkshtshrm4 <parikshit@kazam.in>
2022-04-13 16:33:04 +0530

    initial boiler plate

---
+++ navigator/authNavigator.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const authNavigator = () => {
+  return (
+    <View>
+      <Text>authNavigator</Text>
+    </View>
+  )
+}
+
+export default authNavigator
